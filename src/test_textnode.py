8bb94ef7a47abf64ba2698fd55378662bb6b1cdb
Ruben <gamewalkthrougher@gmail.com>
2026-03-26 21:32:50 +0400

    split delimiter for textnode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -28,6 +28,70 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node lol", TextType.TEXT)
         node2 = TextNode("This is a text node", TextType.TEXT)
         self.assertNotEqual(node, node2)
+
+    def test_text_to_list_delimiter(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.TEXT),
+            ],
+        )
+
+    def test_text_to_list_delimiter_multi(self):
+        node = TextNode(
+            "This is text with two `code block` `code block 2` words", TextType.TEXT
+        )
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is text with two ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" ", TextType.TEXT),
+                TextNode("code block 2", TextType.CODE),
+                TextNode(" words", TextType.TEXT),
+            ],
+        )
+
+    def test_text_to_list_delimiter_incorrect(self):
+        node = TextNode(
+            "This is text with incorrect `code block `code block 2` delimiters",
+            TextType.TEXT,
+        )
+        with self.assertRaises(ValueError):
+            new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+
+    def test_text_to_list_delimiter_multi_input(self):
+        node = TextNode(
+            "This is text with two `code block` `code block 2` words, and _italic_ word",
+            TextType.TEXT,
+        )
+        node2 = TextNode(
+            "This is a text with a **bold block** and _italic_", TextType.TEXT
+        )
+        new_nodes = split_nodes_delimiter([node, node2], "_", TextType.ITALIC)
+        new_nodes = split_nodes_delimiter(new_nodes, "**", TextType.BOLD)
+        new_nodes = split_nodes_delimiter(new_nodes, "`", TextType.CODE)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is text with two ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" ", TextType.TEXT),
+                TextNode("code block 2", TextType.CODE),
+                TextNode(" words, and ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word", TextType.TEXT),
+                TextNode("This is a text with a ", TextType.TEXT),
+                TextNode("bold block", TextType.BOLD),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+            ],
+        )
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -30,3 +30,26 @@ class TextNode:
         if self.text_type is TextType:
             return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def split_nodes_delimiter(
+    old_nodes: list[TextNode], delimiter: str, text_type: TextType
+):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type is not TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        separated = node.text.split(delimiter)
+        if len(separated) % 2 == 0:
+            raise ValueError(f"Markdown node {node} has invalid syntax")
+        for i in range(0, len(separated)):
+            if i == 0 and separated[i] == "":
+                continue
+            if i % 2 == 0:
+                new_nodes.append(TextNode(separated[i], TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(separated[i], text_type))
+
+    return new_nodes
